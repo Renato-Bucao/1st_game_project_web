4ed491be7448545fd48ce44f5e2c54a8bca7b353
Renato-Bucao <renatobucaojr.dev@gmail.com>
2026-05-13 21:16:25 +0800

    chore: setup TypeScript config and Jest testing environment

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,30 @@
+/// <reference types="jest" />
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+//import { App } from 'supertest/types'; //I removed <App> from INestApplication<App> because INestApplication doesn’t take a generic type here.
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
